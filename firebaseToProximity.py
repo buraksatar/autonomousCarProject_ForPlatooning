dd4b167abde8563934083034575386e018ead07b
Burak Satar <burak.satar@ygaventures.org>
2016-08-23 12:28:30 +0300

    Update firebaseToProximity.py

--- firebaseToProximity.py
+++ firebaseToProximity.py
@@ -1,9 +1,7 @@
-# Google Firebase Account
-# https://testautonomous.firebaseio.com/
+# Google Firebase Account https://testautonomous.firebaseio.com/
 # sefSxG47MiRpAoQSRjIL7anAZNad11P1D9GftxI
 
 import time
-
 import pyrebase
 
 config = {
@@ -15,27 +13,20 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-print "Distance Measurement In Progress"
+print "Getting Results In Progress"
 
 time.sleep(1)
 
-a = 1
+db = firebase.database()
+#data = db.child("autonomous").get()
 
-while a < 10000 :
+#print(data.val())
+#print(data.key())
 
-  time.sleep(0.1)
-
-#  db = firebase.database()
-#  distance = round(distance, 2)
-
-#  print "Distance:",distance,"cm"
-
-#  a += 1
-
-#  time.sleep(0.1)
-
-#  db = firebase.database()
-#  db.child("autonomous")
-#  data = {"number": distance}
-#  db.child("autonomous").push(data)
-#  db.update(data)
+def stream_handler(post):
+    print(post["event"]) # put
+    print(post["path"]) # /-K7yGTTEp7O549EzTYtI
+    print(post["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+   # print(post["number"])
+   
+my_stream = db.child("autonomous").stream(stream_handler)

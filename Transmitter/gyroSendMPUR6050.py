216e54858321b311adcbb86c4edbc8c878dd4f07
Burak Satar <burak.satar@ygaventures.org>
2016-08-27 12:50:45 +0300

    Update gyroSendMPUR6050.py

--- Transmitter/gyroSendMPUR6050.py
+++ Transmitter/gyroSendMPUR6050.py
@@ -1,7 +1,19 @@
+
 #!/usr/bin/python
 
+import pyrebase
+import time
 import smbus
 import math
+
+config = {
+  "apiKey": "usefSxG47MiRpAoQSRjIL7anAZNad11P1D9GftxI",
+  "authDomain": "testautonomous.firebaseapp.com",
+  "databaseURL": "https://testautonomous.firebaseio.com",
+  "storageBucket": "testautonomous.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
 
 # Power management registers
 power_mgmt_1 = 0x6b
@@ -26,6 +38,7 @@ def read_word_2c(adr):
 def dist(a,b):
     return math.sqrt((a*a)+(b*b))
 
+
 def get_y_rotation(x,y,z):
     radians = math.atan2(x, dist(y,z))
     return -math.degrees(radians)
@@ -34,38 +47,67 @@ def get_x_rotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
     return math.degrees(radians)
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards AND FOR RASPI 3
+bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
 address = 0x68       # This is the address value read via the i2cdetect command
 
 # Now wake the 6050 up as it starts in sleep mode
 bus.write_byte_data(address, power_mgmt_1, 0)
 
-print "gyro data"
-print "---------"
+#print "gyro data"
+#print "---------"
 
-gyro_xout = read_word_2c(0x43)
-gyro_yout = read_word_2c(0x45)
-gyro_zout = read_word_2c(0x47)
+a = 1
 
-print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+while a < 5000:
 
-print
-print "accelerometer data"
-print "------------------"
+#  print "GYRO DATA"
+  db = firebase.database()
+  gyro_xout = read_word_2c(0x43)
+  gyro_yout = read_word_2c(0x45)
+  gyro_zout = read_word_2c(0x47)
 
-accel_xout = read_word_2c(0x3b)
-accel_yout = read_word_2c(0x3d)
-accel_zout = read_word_2c(0x3f)
+  if gyro_xout < (-4000) and gyro_yout > (4000) and gyro_zout < (-4000):
+    data = {"direction": "Right"}
+    db.update(data)
+    print "RIGHT"
+    time.sleep(1)
 
-accel_xout_scaled = accel_xout / 16384.0
-accel_yout_scaled = accel_yout / 16384.0
-accel_zout_scaled = accel_zout / 16384.0
+  elif gyro_xout > (3000) and gyro_yout < (-3000) and gyro_zout > (3000):
+    data = {"direction": "Left"}
+    db.update(data)
+    print "LEFT"
+    time.sleep(1)
 
-print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+  else:
+#    print "NORMAL"
+    time.sleep(0.001)
 
-print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+#  print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
+#  print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
+#  print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+#  print "XXX : ", gyro_xout
+#  print "YYY : ", gyro_yout
+#  print "ZZZ : ", gyro_zout
+
+  a += 1
+#
+#  print
+#  print "accelerometer data"
+#  print "------------------"
+#
+#  accel_xout = read_word_2c(0x3b)
+#  accel_yout = read_word_2c(0x3d)
+#  accel_zout = read_word_2c(0x3f)
+#
+#  accel_xout_scaled = accel_xout / 16384.0
+#  accel_yout_scaled = accel_yout / 16384.0
+#  accel_zout_scaled = accel_zout / 16384.0
+#
+#  print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
+#  print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
+#  print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+#
+#  print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+#  print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+#
+#  time.sleep(0.5)
